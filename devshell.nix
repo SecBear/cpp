9a385ad93b00830f117339fefa770e56ffc94991
Bryce Thorpe <thorpebryce@gmail.com>
2025-08-27 21:51:14 -0500

    feat: devshell aliases

--- devshell.nix
+++ devshell.nix
@@ -18,10 +18,14 @@ pkgs.mkShell {
 
   # Load custom bash code
   shellHook = ''
+    alias clangd="clang++ -DDEBUG -std=c++23 main.cpp -o main_debug"
+    alias clangr="clang++ -O3 -DNDEBUG -std=c++23 -march=native main.cpp -o main_release"
     echo " ---\ "
     echo " + C++23 development environment"
     echo " + Clang version: $(clang++ --version | head -n1)"
-    echo " + To compile: clang++ -std=c++23 -stdlib=libc++ file.cpp"
+    echo " + To compile: "
+    echo " +  - DEBUG:   clangd"
+    echo " +  - RELEASE: clangr"
     echo " ---/ "
   '';
 }
